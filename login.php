05ba7ac2b814ead4a185973a40cdae0335a9f348
phongsanwa <46067788+phongsanwa@users.noreply.github.com>
2019-02-27 00:12:32 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="login.css">
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="2.png" id="icon" alt="User Icon" style="" />
+      <h1>Pharmacy</h1>
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="tài khoản">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="mật khẩu">
+      <input type="submit" class="fadeIn fourth" value="Đăng Nhập">
+    </form>
+
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+      <a class="underlineHover" href="#">Trang Chủ</a>
+    </div>
+
+  </div>
+</div>
