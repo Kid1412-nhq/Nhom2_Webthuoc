3619e9b830df160630781d24ad57fd08cda7e591
Cao Văn Hoài <46067526+cvhoai@users.noreply.github.com>
2019-03-18 18:12:10 +0700

    Add files via upload

--- choduocphamhapu/views/product/admin.php
+++ choduocphamhapu/views/product/admin.php
@@ -6,7 +6,7 @@
 			<div class="col-xs-6">
 				<form action="seach.php" method="POST">
 					<input type="text" name="txtSeach" id="txtSeach" placeholder="Vui lòng nhập sản phẩm cần tìm" class="">
-					<input type="submit" name="btnSeach" value="Seach" class="glyphicon glyphicon-search" class="btn btn-success">
+					<input type="submit" name="btnSeach" value="Tìm kiếm" class="glyphicon glyphicon-search" class="btn btn-success">
 				</form>
 			</div>
 			<div class="col-xs-6" style="text-align:right;">
@@ -26,25 +26,44 @@
 					<tr>
 						<th>Mã SP</th>
 						<th>Tên SP</th>
-						<th>Giá SP</th>
+						<th>Giá SP (vnđ)</th>
+                        <th>Giá bán (vnđ)</th>
+                        <th>Giá Nhập (vnđ)</th>
 						<th>Số lượng SP</th>
+                        <th>Số lượng nhập</th>
+                        <th>Doanh thu (vnđ)</th>
+                        <th>Lợi nhuận (vnđ)</th>
 						<th>Hình ảnh SP</th>
 						<th>Mã Danh mục</th>
 						<th>Miêu tả</th>
 						<th>Thao tác</th>
 					</tr>
 					<?php
+                    $tong_loinhuan = 0;
+                    $tong_doanhthu = 0;
 					foreach ($data as $k => $v) {
-					
+					    $doanhthu = ($v['Soluongnhap'] - $v['Soluongton'])*$v['Giabanmoi'];
+					    $format_doanhthu = number_format($doanhthu);
+					    $loinhuan = ($v['Soluongnhap'] - $v['Soluongton'])*($v['Giabanmoi'] - $v['Dongianhap']);
+					    $format_loinhuan = number_format($loinhuan);
+					    $tong_doanhthu += $doanhthu;
+					    $format_tong_doanhthu = number_format($tong_doanhthu);
+					    $tong_loinhuan += $loinhuan;
+					    $format_tong_loinhuan = number_format($tong_loinhuan);
 					?>
 					<tr>
 						<td><?php echo $v['MaSP'];?></td>
 						<td><?php echo $v['TenSP'];?></td>
-						<td><?php echo $v['GiaSP'];?></td>
-						<td><?php echo $v['SoluongSP'];?></td>
-						<td><?php echo $v['HinhanhSP'];?></td>
+						<td><?php echo $v['Dongiacu'];?></td>
+                        <td><?php echo $v['Giabanmoi'];?></td>
+                        <td><?php echo $v['Dongianhap'];?></td>
+						<td><?php echo $v['Soluongton'];?></td>
+                        <td><?php echo $v['Soluongnhap'];?></td>
+                        <td><?php echo $format_doanhthu;?></td>
+                        <td><?php echo $format_loinhuan;?></td>
+						<td><?php echo $v['Hinhanh'];?></td>
 						<td><?php echo $v['MaDM'];?></td>
-						<td><?php echo $v['MieutaSP'];?></td>
+						<td><?php echo $v['Mieuta'];?></td>
 						<td>
 							<a href="views/product/update.php?id=<?php echo $v['MaSP'];?>" class="btn btn-default">
 								<span class="glyphicon glyphicon-pencil"></span>
@@ -58,6 +77,9 @@
 						}
 					?>
 				</table>
+                <hr>
+                <h5>Tổng doanh thu: <b><?php echo $format_tong_doanhthu;?></b> vnđ</h5>
+                <h5>Tổng lợi nhuận: <b><?php echo $format_tong_loinhuan;?></b> vnđ</h5>
 			</div>
 		</div>
 	</div>
